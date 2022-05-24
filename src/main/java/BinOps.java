a304c3d57527ea94ea17814782cb3c3472bb8a26
ArtJDev <artemkurilenko1983@yandex.ru>
2022-05-24 11:35:53 +0300

    Initial commit

---
+++ src/main/java/BinOps.java
@@ -0,0 +1,22 @@
+public class BinOps {
+    public String sum(String a, String b) {
+        int x = Integer.parseInt(a, 2);
+        int y = Integer.parseInt(b, 2);
+        int z = x + y;
+        return Integer.toBinaryString(z);
+    /*Переведите числа из двоичной записи в
+      строках в int, сложите и переведите
+      обратно в двоичную запись*/
+    }
+
+    public String mult(String a, String b) {
+        int x = Integer.parseInt(a, 2);
+        int y = Integer.parseInt(b, 2);
+        int z = x * y;
+        return Integer.toBinaryString(z);
+
+    /*Переведите числа из двоичной записи в
+      строках в int, перемножьте и переведите
+      обратно в двоичную запись*/
+    }
+}
